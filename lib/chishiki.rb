24a0482c7f3ad941d1918094c843952ee375db10
--global <works.kvollmer@gmail.com>
2014-04-28 13:42:31 -0400

    merge

--- lib/chishiki.rb
+++ lib/chishiki.rb
@@ -4,31 +4,34 @@
 require 'ffi-ncurses'
 require 'logger'
 include FFI::NCurses
+require './chishiki' #todo: require entire lib folder?
+module Chishiki
   $log = Logger.new("log")
 begin
+  $form = Form.new
+  $form.add Label.new(
+    :pos => Pos.new(5,5))
   initscr
   raw
   start_color
+  use_default_colors 
   keypad stdscr, true
   noecho
   curs_set 1
   ch = 0
   name = "none"
-  newl = getyx stdscr
   # while ch != 27                # Escape
   while ch != KEY_CTRL_Q
     refresh
+    doupdate
     ch = wgetch stdscr
     $log.debug ch
-    if ch == 127
+    if ch == KEY_BACKSPACE || ch == KEY_DC || ch == 127
       px = getyx stdscr
-      $log.debug mvwinch stdscr, px[0], px[1]-1
+      $log.debug px
       mvwdelch stdscr, px[0], px[1]-1
-      move(newl[0], newl[1]) #move to prev saved line, either from carriage return or exceeding text width
+      $log.debug :yup
     else
-      if ch == 10
-        newl = getyx stdscr
-      end
       waddch stdscr, ch
     end
   end
@@ -36,4 +39,5 @@ rescue => err
 ensure
   endwin
   $log.debug err
+end
 end
