62cd58129ffdf76f433330622c62e8ae31e5f182
jollywho <works.kvollmer@gmail.com>
2014-04-28 08:58:39 -0400

    char handling test

--- lib/chishiki.rb
+++ lib/chishiki.rb
@@ -2,24 +2,36 @@
 #
 #
 require 'ffi-ncurses'
+require 'logger'
 include FFI::NCurses
+  $log = Logger.new("log")
 begin
+  
   initscr
   raw
+  start_color
   keypad stdscr, true
   noecho
-  curs_set 0
+  curs_set 1
   ch = 0
   name = "none"
   # while ch != 27                # Escape
   while ch != KEY_CTRL_Q
     refresh
+    doupdate
     ch = wgetch stdscr
-    if ch == KEY_ENTER
-      clear
+    $log.debug ch
+    if ch == KEY_BACKSPACE || ch == KEY_DC || ch == 127
+      px = getyx stdscr
+      $log.debug px
+      mvwdelch stdscr, px[0], px[1]-1
+      $log.debug :yup
+    else
+      waddch stdscr, ch
     end
-    waddch stdscr, ch | A_UNDERLINE
   end
+rescue => err
 ensure
   endwin
+  $log.debug err
 end
